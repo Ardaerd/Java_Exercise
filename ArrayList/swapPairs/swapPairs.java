c397bd115d2496b820a59c0a3db51ad4a03bf354
arda.erdogan <arda.erdogan@ozu.edu.tr>
2021-04-04 23:13:10 +0300

    main method is created and ArrayList is initialized

---
+++ ArrayList/swapPairs/swapPairs.java
@@ -0,0 +1,23 @@
+package ArrayList.swapPairs;
+
+import java.util.ArrayList;
+
+public class swapPairs {
+
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add("be");
+        arr.add("to");
+        arr.add("not");
+        arr.add("or");
+        arr.add("be");
+        arr.add("to");
+        arr.add("-Hamlet");
+        arr.add("that is the question");
+
+        System.out.println(arr);
+    }
+
+
+}
