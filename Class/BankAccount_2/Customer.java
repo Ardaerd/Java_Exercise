2ce244fa73496a48f36d82ba56feb77d9c1ea14c
arda.erdogan <arda.erdogan@ozu.edu.tr>
2021-04-05 19:11:02 +0300

    Customer class is completed

---
+++ Class/BankAccount_2/Customer.java
@@ -0,0 +1,24 @@
+package Class.BankAccount_2;
+
+public class Customer {
+    private String name;
+    private Account account;
+
+    public Customer(String name, Account account) {
+        this.name = name;
+        this.account = account;
+    }
+
+    public void deposit(double amount, String currency) {
+        this.account.deposit(amount, currency);
+    }
+
+    public void withdraw(double amount) {
+        this.account.withdraw(amount);
+    }
+
+    public void report() {
+        System.out.println("Customer " + this.name + " ");
+        this.account.report();
+    }
+}
