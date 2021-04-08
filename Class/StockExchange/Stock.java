fad6222831c4776466cf1322a370626670986052
arda.erdogan <arda.erdogan@ozu.edu.tr>
2021-04-08 11:38:39 +0300

    Constructor and purchase method are initialized

--- Class/StockExchange/Stock.java
+++ Class/StockExchange/Stock.java
@@ -5,11 +5,27 @@ public class Stock {
     private int totalShares;
     private double totalCost;
 
+    public Stock(String symbol, int totalShares, double totalCost) {
+        if (symbol == null)
+            throw new NullPointerException();
+        this.symbol = symbol;
+        this.totalShares = totalShares;
+        this.totalCost = totalCost;
+    }
+
+    // Initializes a new stock with no shares purchased
+    public Stock(String symbol) {
+        this(symbol,0,0.0);
+    }
+
     public double getProfit(double currentPrice) {
         return (currentPrice - totalCost) * totalShares;
     }
 
-    public void purchase(int shares, pricePerShare) {
-        
+    // Record a purchase of the given number of shares of this stock
+    // at the given price per share
+    public void purchase(int shares, double pricePerShare) {
+        totalShares += shares;
+        totalCost += pricePerShare * shares;
     }
 }
