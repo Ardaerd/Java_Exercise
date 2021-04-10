7713a6b956ff9d5b35aa55647c71c0a98e900bfc
Ardaerd <arderd@outlook.com>
2021-04-10 14:14:36 +0300

    Employee class is initialized

---
+++ Inheritance_and_Interface/LawFirm_HiringDepartment/Employee.java
@@ -0,0 +1,21 @@
+package Inheritance_and_Interface.LawFirm_HiringDepartment;
+
+// A class to represent employees in general.
+public class Employee {
+
+    public int getHours() {
+        return 40;
+    }
+
+    public double getSalary() {
+        return 40_000.0;
+    }
+
+    public int getVacationDays() {
+        return 10;
+    }
+
+    public String getVacationForm() {
+        return "yellow";
+    }
+}
