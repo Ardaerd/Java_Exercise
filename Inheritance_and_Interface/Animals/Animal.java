bd3cdd49b5556423202e33ee6ba298365da68cce
Ardaerd <arderd@outlook.com>
2021-04-15 22:06:30 +0300

    Animal class is completed

--- Inheritance_and_Interface/Animals/Animal.java
+++ Inheritance_and_Interface/Animals/Animal.java
@@ -2,7 +2,61 @@ package Inheritance_and_Interface.Animals;
 
 public class Animal {
     private String food;
-    private String hunger;
-    private String location;
-    
+    private String name;
+    private String habitat;
+    private String color;
+
+    public Animal(String food, String name, String habitat, String color) {
+        this.food = food;
+        this.name = name;
+        this.habitat = habitat;
+        this.color = color;
+    }
+
+    public void makeNoise(String noise) {
+        System.out.println(name + " makes a sound like that " + noise);
+    }
+
+    public void eat() {
+        System.out.println(name + " eats " + food);
+    }
+
+    public void sleep(boolean sleep) {
+        if (sleep)
+            System.out.println("Watch out " + name + " is sleeping");
+        else
+            System.out.println(name + " is waking up. Runn!!!");
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
