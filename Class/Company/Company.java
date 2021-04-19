b9056e35acf53e90229093422becc88b7f27083e
Ardaerd <arderd@outlook.com>
2021-04-19 12:54:27 +0300

    addDepartment method and Company constructor is implemented

--- Class/Company/Company.java
+++ Class/Company/Company.java
@@ -1,6 +1,19 @@
 package Class.Company;
 
+import java.util.ArrayList;
+
 public class Company {
     private String name;
+    private ArrayList<Department> departments;
 
+    public Company(String name) {
+        this.name = name;
+        departments = new ArrayList<Department>();
+    }
+
+    public void addDepartment(Department department) {
+        departments.add(department);
+    }
+
+    
 }
--- Class/Company/Department.java
+++ Class/Company/Department.java
@@ -1,4 +1,5 @@
 package Class.Company;
 
 public class Department {
+
 }
