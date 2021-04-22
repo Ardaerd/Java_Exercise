229839e311281e154b8d40c0010b1ab32be8414a
Ardaerd <arderd@outlook.com>
2021-04-23 00:05:30 +0300

    X5 is initalized

--- Inheritance_and_Interface/Gallery/Car/BMW/X5.java
+++ Inheritance_and_Interface/Gallery/Car/BMW/X5.java
@@ -1,4 +1,12 @@
 package Inheritance_and_Interface.Gallery.Car.BMW;
 
-public class X5 {
+public class X5 extends BMW{
+
+    public X5() {
+        super(105_000.00,230.00,4.3,2021,"X5 M","Oil","Tanzanite Blue II Metalic");
+    }
+
+    public String toString() {
+       return super.toString();
+    }
 }
