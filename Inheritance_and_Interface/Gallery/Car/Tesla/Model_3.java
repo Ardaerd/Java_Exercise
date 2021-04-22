2223a6d6251264a5a4e2501c3b7ab1d79106beee
Ardaerd <arderd@outlook.com>
2021-04-22 09:00:15 +0300

    Model_3 is added

---
+++ Inheritance_and_Interface/Gallery/Car/Tesla/Model_3.java
@@ -0,0 +1,12 @@
+package Inheritance_and_Interface.Gallery.Car.Tesla;
+
+public class Model_3 extends Tesla{
+
+    public Model_3() {
+        super(30.690,225.30,5.3,2021,"Model_3","Black");
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
