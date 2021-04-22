5735a53b50d1574cea19ab1f8bf464a967d38035
Ardaerd <arderd@outlook.com>
2021-04-22 09:03:26 +0300

    Model_X is added

---
+++ Inheritance_and_Interface/Gallery/Car/Tesla/Model_X.java
@@ -0,0 +1,12 @@
+package Inheritance_and_Interface.Gallery.Car.Tesla;
+
+public class Model_X extends Tesla{
+
+    public Model_X() {
+        super(83.190,250.0,3.8,2021,"Model_X","Red");
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
