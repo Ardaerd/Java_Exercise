860346a9c37d3983db1fc94b429eb40bf3b93eea
Ardaerd <arderd@outlook.com>
2021-04-23 00:25:07 +0300

    Gallery Project is completed

---
+++ Inheritance_and_Interface/Gallery/Car/BMW/Z4.java
@@ -0,0 +1,12 @@
+package Inheritance_and_Interface.Gallery.Car.BMW;
+
+public class Z4 extends BMW{
+
+    public Z4() {
+        super(63_700.00,250.00,5.4,2021,"Z4 M","Oil","White");
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
--- Inheritance_and_Interface/Gallery/Main.java
+++ Inheritance_and_Interface/Gallery/Main.java
@@ -1,9 +1,14 @@
 package Inheritance_and_Interface.Gallery;
 
+import Inheritance_and_Interface.Gallery.Car.BMW.M4;
+import Inheritance_and_Interface.Gallery.Car.BMW.X5;
+import Inheritance_and_Interface.Gallery.Car.BMW.Z4;
 import Inheritance_and_Interface.Gallery.Car.Car;
 import Inheritance_and_Interface.Gallery.Car.Tesla.Model_3;
 import Inheritance_and_Interface.Gallery.Car.Tesla.Model_S;
 import Inheritance_and_Interface.Gallery.Car.Tesla.Model_X;
+
+import java.util.concurrent.Callable;
 
 public class Main {
 
@@ -50,6 +55,40 @@ public class Main {
         System.out.println(customer_2);
         System.out.println(customer_3);
 
+        Customer customer_4 = new Customer("Bill Gates",2_000_000.0,gallery_1);
+        Customer customer_5 = new Customer("Mark Zuckerburg",200_000.0,gallery_1);
+
+        Car M4 = new M4();
+        Car X5 = new X5();
+        Car Z4 = new Z4();
+
+        System.out.println(M4);
+        System.out.println(X5);
+        System.out.println(Z4);
+
+        M4.arrivalTime(500);
+        X5.arrivalTime(500);
+        Z4.arrivalTime(500);
+
+        gallery_1.addVehicles(M4);
+        gallery_1.addVehicles(X5);
+        gallery_1.addVehicles(Z4);
+
+        gallery_1.listOfOption();
+
+        gallery_1.sold(6,customer_1);
+        gallery_1.sold(4,customer_4);
+        gallery_1.sold(5,customer_5);
+        gallery_1.sold(6,customer_5);
+
+        gallery_1.listOfCustomer();
+        gallery_1.listOfOption();
+        gallery_1.listOfSoldVehicles();
+
+        System.out.println(gallery_1);
+
+        System.out.println(customer_4);
+        System.out.println(customer_5);
     }
 
 }
