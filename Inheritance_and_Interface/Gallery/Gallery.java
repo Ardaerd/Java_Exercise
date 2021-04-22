0a3712cadb9e360f54899acdc3c7127abf0f9d86
Ardaerd <arderd@outlook.com>
2021-04-22 09:25:48 +0300

    Some method is added in Gallery class

--- Inheritance_and_Interface/Gallery/Gallery.java
+++ Inheritance_and_Interface/Gallery/Gallery.java
@@ -1,4 +1,32 @@
 package Inheritance_and_Interface.Gallery;
 
+import java.util.ArrayList;
+
 public class Gallery {
+
+    private double revenue;
+    private String name;
+    private ArrayList<Vehicle> vehicles;
+
+    public Gallery(String name, double revenue) {
+        this.name = name;
+        this.revenue = revenue;
+        vehicles = new ArrayList<Vehicle>();
+    }
+
+    public void addVehicles(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+    public void listOfOption() {
+        System.out.println("------ Welcome to " + name + " ------");
+        for (int i = 0; i < vehicles.size(); i++) {
+            System.out.println(1+i + "- " + vehicles.get(i).getModel() + " (" + vehicles.get(i).getBrand() + ")");
+        }
+        System.out.println("---------------------------------------");
+    }
+
+    public void selling(int option) {
+        revenue += vehicles.get(option-1).getPrice();
+    }
 }
